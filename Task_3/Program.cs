8a433edc5176f3d7ad072b4af7943d0a60fde058
etitievsky <etitievsky@mail.ru>
2022-12-17 15:44:13 +0300

    Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов
+
+// Методы
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+void WriteTable(int n)
+{
+    int i = 1;
+    double resalt = 0;
+    while(i <= n)
+    {
+        resalt = Math.Pow(i, 3);
+        Console.Write(resalt+", ");
+        i = i + 1;
+    }
+}
+
+Console.Clear();
+// Запросим число N
+int n = EnterData("Введите число N: ");
+// Вывод информации
+WriteTable(n);
