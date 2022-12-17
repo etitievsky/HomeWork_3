b695c6235d32a87c78a1442dae1ba34a96e0bb60
etitievsky <etitievsky@mail.ru>
2022-12-17 13:52:37 +0300

    Task 01

---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+string EnterData(string text)
+{
+    Console.Write(text);
+    string number = Console.ReadLine();
+    return number;
+}
+void CheckPalindrom(string number)
+{
+    if (number[0]==number[4] & number[1]==number[3]) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");
+}
+
+Console.Clear();
+string number = EnterData("Введите пятизначное число: ");
+CheckPalindrom(number);
